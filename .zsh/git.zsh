0165196868954546508f18c276bc72da3e916ed2
adust09 <shoukitsuda@icloud.com>
2024-12-08 01:04:24 +0900

    refactoring fzf

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -5,3 +5,31 @@ alias ghp='cat ~/git_in_omz.text'
 alias gtr='git log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
 alias commitfilesname='git show commit_id --name-only'
 alias fgbd='git branch | fzf -m | xargs -I {} git branch -D {}'
+
+# fbr - checkout git branch (including remote branches)
+fbr() {
+    local branches branch
+    branches=$(git branch --all | grep -v HEAD) &&
+    branch=$(echo "$branches" |
+            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+    autoload -Uz compinit
+    compinit
+fi
+
+# fshow - git commit browser
+fshow() {
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+        --bind "ctrl-m:execute:
+                    (grep -o '[a-f0-9]\{7\}' | head -1 |
+                    xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                    {}
+    FZF-EOF"
+}
