fe287255d8b386b00e8e0890eb75d88b98dddfeb
adust09 <shoukitsuda@icloud.com>
2026-03-05 10:51:35 +0900

    delete unused

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,10 +1,3 @@
-# chat GPT CLI
-alias chat='cd ~/dotfiles/plugins/chatgpt-cli && python3 chatgpt.py'
-alias bu='source ~/dotfiles/env.sh && python3.13 ~/dotfiles/interactive_agent.py'
-
-alias zshconfig="code ~/dotfiles"
-alias zshreload="source ~/dotfiles/.zshrc"
-
 # go
 alias gob='go build'
 alias gor='go run'
